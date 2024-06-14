97866400123648aa84eabd0bbb1f589c39d078bd
akshithcsueb <akatragada@horizon.csueastbay.edu>
2024-06-14 15:58:04 -0700

    Completed two LeetCode Questions of today 6/14/2024

---
+++ CSharp_Practice/LeetCodeSolutions/06-15-2024/RotateString.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_15_2024
+{
+    public class RotateString
+    {
+        public static bool RotateStringValue(string s, string goal)
+        {
+            if((s.Length == 0 && goal.Length != 0) || (s.Length != 0 && goal.Length == 0))
+            {
+                return false;
+            }
+
+            string firstSubWord;
+            int firstSubWordindex;
+
+            string secondSubWord;
+            string fullGoalWord;
+
+            if(s.Length != 0)
+            {
+                if (s.Contains(goal[0]))
+                {
+                    List<int> goalIndexes = new List<int>();
+
+                    for (int i=0; i<s.Length; i++)
+                    {
+                        if(s[i] == goal[0])
+                        {
+                            goalIndexes.Add(i);
+                        }
+                    }
+
+                    for (int i=0; i<goalIndexes.Count; i++)
+                    {
+                        firstSubWordindex = goalIndexes[i];
+                        firstSubWord = s.Substring(firstSubWordindex);
+
+                        secondSubWord = s.Substring(0, firstSubWordindex);
+
+                        fullGoalWord = firstSubWord + secondSubWord;
+
+                        if (goal == fullGoalWord)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+
+        }
+
+        public static void Main()
+        {
+            string s = "bbbacddceeb";
+            string goal = "ceebbbbacdd";
+
+            bool output = RotateStringValue(s, goal);
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ CSharp_Practice/LeetCodeSolutions/06-15-2024/ValidAnagram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_15_2024
+{
+    public class ValidAnagram
+    {
+        public static bool ValidAnagramCheck(string s, string t)
+        {
+            Dictionary<char, int> anagramChars = new Dictionary<char, int>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(!anagramChars.ContainsKey(s[i]))
+                {
+                    anagramChars.Add(s[i], 1);
+                }
+                else
+                {
+                    anagramChars[s[i]]++;
+                }
+            }
+
+            
+            for(int i = 0; i < t.Length; i++)
+            {
+                if(anagramChars.ContainsKey(t[i]))
+                {
+                    anagramChars[t[i]]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in anagramChars)
+            {
+                if(kvp.Value != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static void Main()
+        {
+            string s = "anagram";
+            string t = "nagaram";
+
+            bool output = ValidAnagramCheck(s, t);
+
+            Console.WriteLine(output);
+        }
+    }
+}
