dc088666a84035a49f76582f2de8c1478e48f306
akshithcsueb <akatragada@horizon.csueastbay.edu>
2024-06-18 13:54:03 -0700

    Solved two coding questions

---
+++ CSharp_Practice/LeetCodeSolutions/06-18-2024/CommonWords.cs
@@ -0,0 +1,88 @@
+﻿/*
+ 
+Count Common Words With One Occurrence
+
+Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+Example 1:
+Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+
+Example 2:
+Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+Output: 0
+Explanation: There are no strings that appear in each of the two arrays.
+
+Example 3:
+Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+Output: 1
+Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+
+My Approach - 
+1) created Dictionary
+2) added first array words into it, if exist just put -1 as value, if not 1 as value
+3) decrement values of words that match in first and second array
+4) iterate over the dictionary and i am just looking for values that have 0 and counting them
+ 
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_18_2024
+{
+    public class CommonWords
+    {
+        public static int CountCommonWords(string[] words1, string[] words2)
+        {
+            int count = 0;
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            for (int i = 0; i < words1.Length; i++)
+            {
+                if (words.ContainsKey(words1[i]))
+                {
+                    words[words1[i]] = -1;
+                }
+                else
+                {
+                    words.Add(words1[i], 1);
+                }
+            }
+
+            for (int i = 0; i < words2.Length; i++)
+            {
+                if (words.ContainsKey(words2[i]))
+                {
+                    words[words2[i]]--;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> kvp in words)
+            {
+                if(kvp.Value == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] words1 = { "", "" };
+            string[] words2 = { "" };
+
+            int output = CountCommonWords(words1, words2);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ CSharp_Practice/LeetCodeSolutions/06-18-2024/PrefixOfGivenString.cs
@@ -0,0 +1,81 @@
+﻿/*
+ 
+Count Prefixes of a Given String
+
+You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+
+Return the number of strings in words that are a prefix of s.
+
+A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+
+ 
+
+Example 1:
+Input: words = ["a","b","c","ab","bc","abc"], s = "abc"
+Output: 3
+Explanation:
+The strings in words which are a prefix of s = "abc" are:
+"a", "ab", and "abc".
+Thus the number of strings in words which are a prefix of s is 3.
+
+Example 2:
+Input: words = ["a","a"], s = "aa"
+Output: 2
+Explanation:
+Both of the strings are a prefix of s. 
+Note that the same string can occur multiple times in words, and it should be counted each time.
+ 
+
+My Approach - 
+1) Iterating over words arrays and in that iterating over each word
+2) comparing each letter in the word array to the s string
+3) break the checking if word letters and s letters doesn't match
+4) if u reach end letter of word and if it is same then update count
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_18_2024
+{
+    public class PrefixOfGivenString
+    {
+        public static int CountPrefixes(string[] words, string s)
+        {
+            int count = 0;
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                for(int j = 0; j < words[i].Length; j++)
+                {
+                    if(j < s.Length)
+                    {
+                        if (words[i][j] != s[j])
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            if (words[i].Length - 1 == j)
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] words = { "e", "pplp", "rt", "xyz", "l", "brn", "l", "l", "l", "l", "l", "l", "tw", "l", "zjyd", "lj", "yfy", "za", "l", "l", "l", "l", "l", "l", "lfnb", "l", "sc", "l", "vu", "o", "l" };
+            string s = "l";
+
+            int output = CountPrefixes(words, s);
+
+            Console.WriteLine(output);
+        }
+    }
+}
