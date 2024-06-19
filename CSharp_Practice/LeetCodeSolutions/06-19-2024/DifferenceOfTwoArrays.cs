de3cedffb3ed870f9c26ccd6613da4b182a34ac6
akshithcsueb <akatragada@horizon.csueastbay.edu>
2024-06-19 11:44:53 -0700

    Solved 2 leetcode quesitons

---
+++ CSharp_Practice/LeetCodeSolutions/06-19-2024/DifferenceOfTwoArrays.cs
@@ -0,0 +1,107 @@
+﻿/*
+ 
+ Find the Difference of Two Arrays
+
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+
+ 
+
+Example 1:
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+
+Example 2:
+Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+Output: [[3],[]]
+Explanation:
+For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+ 
+
+My Approach - 
+1) created two lists
+2) used two nested for loops to check values that doesn't exist in between them
+3) for each value in first array go until last element in second array and if it doesn't exist
+then also check that is not already present in the list if these satisfied then add to the list,
+also if values matches then it will break the check.
+4) finally created main array that has these two arrays and returned it
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_19_2024
+{
+    public class DifferenceOfTwoArrays
+    {
+        public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            IList<int> answer0 = new List<int>();
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1[i] == nums2[j])
+                    {
+                        break;
+                    }
+
+                    if(j == nums2.Length-1 && !answer0.Contains(nums1[i]))
+                    {
+                        answer0.Add(nums1[i]);
+                    }
+                }
+            }
+
+            IList<int> answer1 = new List<int>();
+
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                for (int j = 0; j < nums1.Length; j++)
+                {
+                    if (nums2[i] == nums1[j])
+                    {
+                        break;
+                    }
+
+                    if (j == nums1.Length - 1 && !answer1.Contains(nums2[i]))
+                    {
+                        answer1.Add(nums2[i]);
+                    }
+                }
+            }
+
+            IList<IList<int>> answer = [answer0, answer1];
+
+            return answer;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 3 };
+            int[] nums2 = { 2, 4, 6 };
+
+            IList<IList<int>> output = FindDifference(nums1, nums2);
+
+            foreach (IList<int> i in output)
+            {
+                foreach (int j in i)
+                {
+                    Console.WriteLine(j);
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+    
+}
---
+++ CSharp_Practice/LeetCodeSolutions/06-19-2024/FindThePeaks.cs
@@ -0,0 +1,77 @@
+﻿/*
+ 
+ Find the Peaks
+
+You are given a 0-indexed array mountain. Your task is to find all the peaks in the mountain array.
+
+Return an array that consists of indices of peaks in the given array in any order.
+
+Notes:
+
+A peak is defined as an element that is strictly greater than its neighboring elements.
+The first and last elements of the array are not a peak.
+ 
+
+Example 1:
+Input: mountain = [2,4,4]
+Output: []
+Explanation: mountain[0] and mountain[2] can not be a peak because they are first and last elements of the array.
+mountain[1] also can not be a peak because it is not strictly greater than mountain[2].
+So the answer is [].
+
+Example 2:
+Input: mountain = [1,4,3,8,5]
+Output: [1,3]
+Explanation: mountain[0] and mountain[4] can not be a peak because they are first and last elements of the array.
+mountain[2] also can not be a peak because it is not strictly greater than mountain[3] and mountain[1].
+But mountain [1] and mountain[3] are strictly greater than their neighboring elements.
+So the answer is [1,3].
+
+My Approach -
+1) created a list
+2) if length of array is only 2 then i will return empty list
+3) for loop with a condition where previous element should be less than current element and 
+current element should be greater than the next element, if satisified I will add
+ 
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_19_2024
+{
+    public class FindThePeaks
+    {
+        public static IList<int> FindPeaks(int[] mountain)
+        {
+            IList<int> result = new List<int>();
+
+            if(mountain.Length <= 2)
+            {
+                return result;
+            }
+
+            for(int i = 1; i <  mountain.Length-1; i++)
+            {
+                if (mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1])
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] mountain = { 1,4,3,8,5};
+
+            IList<int> output = FindPeaks(mountain);
+
+            for(int i = 0; i < output.Count; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+    }
+}
