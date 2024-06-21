475c2cd49e5d1d95a652c2368a339fb29b8eed31
akshithcsueb <akatragada@horizon.csueastbay.edu>
2024-06-21 15:18:39 -0700

    Solved few coding questions

---
+++ CSharp_Practice/LeetCodeSolutions/06-21-2024/MoveZeroes.cs
@@ -0,0 +1,62 @@
+﻿/*
+ 
+Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+My Approach -
+1) Used two for loops, one for 0 pointer and another for not 0 pointer
+2) if 0 is found then it will check for not 0 value and swap them and break the loop
+
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_21_2024
+{
+    public class MoveZeroes
+    {
+        public static void MoveZeroesOperation(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    for(int j = i;  j < nums.Length; j++)
+                    {
+                        if(nums[j] != 0)
+                        {
+                            int temp = nums[i];
+                            nums[i] = nums[j];
+                            nums[j] = temp;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for(int i = 0;i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+
+            MoveZeroesOperation(nums);
+        }
+    }
+}
---
+++ CSharp_Practice/LeetCodeSolutions/06-21-2024/ValidBoomerang.cs
@@ -0,0 +1,53 @@
+﻿/*
+ 
+Valid Boomerang
+
+Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+
+A boomerang is a set of three points that are all distinct and not in a straight line.
+
+Example 1:
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+
+Example 2:
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+
+My Approach -
+1) Initalized variables from the array
+2) Just used formula to find whether valid boomerang or not
+
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_21_2024
+{
+    public class ValidBoomerang
+    {
+        public static bool IsBoomerang(int[][] points)
+        {
+            int x1 = points[0][0];
+            int y1 = points[0][1];
+            int x2 = points[1][0];
+            int y2 = points[1][1];
+            int x3 = points[2][0];
+            int y3 = points[2][1];
+
+            int area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+
+            return area != 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[][] points = [[0, 0], [2, 1], [2, 1]];
+
+            bool output = IsBoomerang(points);
+
+            Console.WriteLine(output);
+        }
+    }
+}
