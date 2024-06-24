555f34fc3191731bb76d5cf3186121734f47ba0e
akshithcsueb <akatragada@horizon.csueastbay.edu>
2024-06-24 12:38:16 -0700

    Solved few coding problems

---
+++ CSharp_Practice/LeetCodeSolutions/06-24-2024/RelativeSortArray.cs
@@ -0,0 +1,88 @@
+﻿/*
+ 
+Relative Sort Array
+
+Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+ 
+
+Example 1:
+Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+Output: [2,2,2,1,4,3,3,9,6,7,19]
+
+Example 2:
+Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
+Output: [22,28,8,6,17,44]
+
+My Approach - 
+1) First I sorted values based on relative position and then I sorted remaining values in
+the array 1
+2) In relative sorting, if array 2 value matches with array 1 then it will swap the founded value
+with the relativeSortPosition index value
+3) In normal sorting I am basically using bubble sort approach
+ 
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_24_2024
+{
+    public class RelativeSortArray
+    {
+        public static int[] SortArray(int[] arr1, int[] arr2)
+        {
+            int relativeSortPosition = 0;
+
+            for(int i = 0; i < arr2.Length; i++)
+            {
+                int relativeSortValue = arr2[i];
+
+                for(int j = 0; j < arr1.Length; j++)
+                {
+                    if(arr1[j] == relativeSortValue)
+                    {
+                        int temp = arr1[relativeSortPosition];
+                        arr1[relativeSortPosition] = arr1[j];
+                        arr1[j] = temp;
+                        relativeSortPosition++;
+                    }
+                }
+            }
+
+            if(arr1.Length > arr2.Length)
+            {
+                for(int i=relativeSortPosition; i<arr1.Length-1; i++)
+                {
+                    for(int j=relativeSortPosition; j<arr1.Length-1; j++)
+                    {
+                        if (arr1[j] > arr1[j+1])
+                        {
+                            int temp = arr1[j];
+                            arr1[j] = arr1[j + 1];
+                            arr1[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+
+            return arr1;
+        }
+
+        // [3,5,5,1,1,7,   2,9,4,6,2]
+
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19 };
+            int[] arr2 = { 2, 1, 4, 3, 9, 6 };
+
+            int[] output = SortArray(arr1, arr2);
+
+            for(int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+    }
+}
---
+++ CSharp_Practice/LeetCodeSolutions/06-24-2024/UniqueNumOfOccurences.cs
@@ -0,0 +1,84 @@
+﻿/*
+ 
+ Unique Number of Occurrences
+
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+
+My Approach -
+1) Created a dictionary to store each value in the array as a key and it's no of times in
+the array as value
+2) next iterating over the the dictionary values to check count of each value, and if 
+greater than 1 then return false otherwise i will return true.
+ 
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_24_2024
+{
+    public class UniqueNumOfOccurences
+    {
+        public static bool UniqueOccurences(int[] arr)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(dic.ContainsKey(arr[i]))
+                {
+                    dic[arr[i]]++;
+                }
+                else
+                {
+                    dic.Add(arr[i], 1);
+                }
+            }
+
+            foreach(KeyValuePair<int, int> kvp in dic)
+            {
+                int count = 0;
+                int value = kvp.Value;
+
+                foreach(KeyValuePair<int, int> kvpValue in dic)
+                {
+                    if(value == kvpValue.Value)
+                    {
+                        count++;
+                    }
+                }
+
+                if(count > 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2 };
+
+            bool output = UniqueOccurences(arr);
+
+            Console.WriteLine(output);
+        }
+    }
+}
