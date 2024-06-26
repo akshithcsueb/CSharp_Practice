861b2700a41443a5706131cdda5282fac7f629a1
akshithcsueb <akatragada@horizon.csueastbay.edu>
2024-06-25 17:25:50 -0700

    Solved few coding problems

---
+++ CSharp_Practice/LeetCodeSolutions/06-25-2024/CommonCharacters.cs
@@ -0,0 +1,115 @@
+﻿/*
+ 
+ Find Common Characters
+
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+Example 1:
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+
+Example 2:
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+My Approach - 
+1) Took a dictionary and added all letters and it's count to it for just first word in
+the array alone
+2) From the second word I added all it's letters and it's count to the new dictionary, 
+and for each next word this dictionary will be overwriten
+3) From the second word after adding to dictionary, we will compare these two dictionaries,
+I iterated over first dictionary and check the values of this key in both dictionaries,
+If tempValues dictionary value is lesser than charValues dictionary value then I will
+replace the tempValues dictionary value of that key to the charValues dictionary value of that key
+4) Finally I will iterate over the charValues dictionary and push those letters to the list
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_25_2024
+{
+    public class CommonCharacters
+    {
+        public static IList<string> FindCommonChars(string[] words)
+        {
+            Dictionary<char, int> charValues = new Dictionary<char, int>();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                Dictionary<char, int> tempValues = new Dictionary<char, int>();
+
+                for(int j = 0; j < words[i].Length; j++)
+                {
+                    if(i == 0)
+                    {
+                        if (!charValues.ContainsKey(words[i][j]))
+                        {
+                            charValues.Add(words[i][j], 1);
+                        }
+                        else
+                        {
+                            charValues[words[i][j]]++;
+                        }
+                    }
+                    else
+                    {
+                        if (!tempValues.ContainsKey(words[i][j]))
+                        {
+                            tempValues.Add(words[i][j], 1);
+                        }
+                        else
+                        {
+                            tempValues[words[i][j]]++;
+                        }
+                    }
+                }
+
+                if(i != 0)
+                {
+                    foreach (KeyValuePair<char, int> kvp in charValues)
+                    {
+                        if (tempValues.ContainsKey(kvp.Key))
+                        {
+                            if (tempValues.TryGetValue(kvp.Key, out int value))
+                            {
+                                if (value < kvp.Value)
+                                {
+                                    charValues[kvp.Key] = value;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            charValues.Remove(kvp.Key);
+                        }
+                    }
+                }
+            }
+
+            IList<string> result = new List<string>();
+
+            foreach(KeyValuePair<char, int> kvp in charValues)
+            {
+                for(int i=0; i < kvp.Value; i++)
+                {
+                    result.Add(kvp.Key.ToString());
+                }
+            }
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] words = { "bella", "label", "roller" };
+
+            IList<string> output = FindCommonChars(words);
+
+            for(int i = 0;i < output.Count;i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+    }
+}
---
+++ CSharp_Practice/LeetCodeSolutions/06-25-2024/CountWordsWithPrefix.cs
@@ -0,0 +1,58 @@
+﻿/*
+ 
+Counting Words With a Given Prefix
+
+You are given an array of strings words and a string pref.
+
+Return the number of strings in words that contain pref as a prefix.
+
+A prefix of a string s is any leading contiguous substring of s.
+
+Example 1:
+Input: words = ["pay","attention","practice","attend"], pref = "at"
+Output: 2
+Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+
+Example 2:
+Input: words = ["leetcode","win","loops","success"], pref = "code"
+Output: 0
+Explanation: There are no strings that contain "code" as a prefix.
+ 
+My Approach - 
+1) Using for loop going over the words in array and just checking if the prefix contains or not in the string starting
+using string method.
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_25_2024
+{
+    public class CountWordsWithPrefix
+    {
+        public static int CountWords(string[] words, string pref)
+        {
+            int count = 0;
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                if (words[i].StartsWith(pref))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] words = { "pay", "attention", "practice", "attend" };
+            string prefix = "at";
+
+            int output = CountWords(words, prefix);
+
+            Console.WriteLine(output);
+        }
+    }
+}
