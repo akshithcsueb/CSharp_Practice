c8814e4ea107d6a7731484fedbfefaa313e20b87
akshithcsueb <akatragada@horizon.csueastbay.edu>
2024-06-27 17:31:59 -0700

    Solved few coding problems

---
+++ CSharp_Practice/LeetCodeSolutions/06-27-2024/DivideArrayIntoEqualPairs.cs
@@ -0,0 +1,87 @@
+﻿/*
+ 
+ Divide Array Into Equal Pairs
+
+You are given an integer array nums consisting of 2 * n integers.
+
+You need to divide nums into n pairs such that:
+
+Each element belongs to exactly one pair.
+The elements present in a pair are equal.
+Return true if nums can be divided into n pairs, otherwise return false.
+
+Example 1:
+Input: nums = [3,2,3,2,2,2]
+Output: true
+Explanation: 
+There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: 
+There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition.
+ 
+
+My Approach - 
+1) First I am checking whether I can divide the size of array by 2 or not
+2) If I am able to divide then I am using dictionary and inserting all values of array
+as keys and it's frequencies
+3) Finally I am trying to divide the values of each dictionary key by 2 or not, if I get not 0
+for modulo operation then I will return false else return true.
+
+
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_27_2024
+{
+    public class DivideArrayIntoEqualPairs
+    {
+        public static bool DivideArray(int[] nums)
+        {
+            Dictionary<int, int> values = new Dictionary<int, int>();
+
+            if((nums.Length % 2) == 0)
+            {
+                for(int i = 0; i < nums.Length; i++)
+                {
+                    if (!values.ContainsKey(nums[i]))
+                    {
+                        values.Add(nums[i], 1);
+                    }
+                    else
+                    {
+                        values[nums[i]]++;
+                    }
+                }
+
+                foreach(KeyValuePair<int, int> kvp in values)
+                {
+                    if((kvp.Value % 2) != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 3, 2, 2, 2 };
+
+            bool output = DivideArray(nums);
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ CSharp_Practice/LeetCodeSolutions/06-27-2024/SortArrayByParity.cs
@@ -0,0 +1,70 @@
+﻿/*
+ 
+ Sort Array By Parity
+
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+Example 1:
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+ 
+
+My Approach - 
+1) I took two index, one at 0 for even numbers and the another at array length - 1 for
+odd numbers
+2) created a new array to insert these values
+3) Iterated over array and if value is even then insert at even index and increment it,
+if it's odd then insert at odd index and decrement it.
+ */
+
+
+using System;
+
+namespace CSharp_Practice.LeetCodeSolutions._06_27_2024
+{
+    public class SortArrayByParity
+    {
+        public static int[] SortArray(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            int evenIndex = 0;
+            int oddIndex = nums.Length - 1;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if ((nums[i] % 2) == 0)
+                {
+                    result[evenIndex] = nums[i];
+                    evenIndex++;
+                }
+                else
+                {
+                    result[oddIndex] = nums[i];
+                    oddIndex--;
+                }
+            }
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = { 3, 1, 2, 4 };
+
+            int[] output = SortArray(nums);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+    }
+}
